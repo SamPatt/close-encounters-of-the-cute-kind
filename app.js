2bee0cd6753026a33ea3d70292fb630e29e51fab
SamPatt <git@sampatt.com>
2023-10-27 23:09:38 -0400

    Add map and player constants, stubbing out functions

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 console.log('We are here!')
+
+/*----- constants -----*/
+const MAP_LEVEL_ONE = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 3, 0, 1, 0, 1, 1, 1, 1, 1, 0, 5, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 4, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 4, 0, 5, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+const PLAYER_START = {
+    lives: 3,
+    creaturesFound: 0
+}
+
+/*----- state variables -----*/
+let maze = MAP_LEVEL_ONE // Sets the maze as a copy of the MAP_LEVEL_ONE array
+const player = PLAYER_START // Sets the player object as a copy of the PLAYER_START object
+
+
+
+
+
+/*----- cached elements  -----*/
+const mazeEl = document.querySelector('#maze')
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+function movePlayer(){
+    
+}
+
+function renderMaze(){
+
+}
+
+function renderPhoto(){
+
+}
+
+function render(){
+
+}
+
+function init(){
+
+}
