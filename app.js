4e5f7a1689189c01b6f7b8e7b9ee2170729a68fa
SamPatt <git@sampatt.com>
2023-10-28 01:08:57 -0400

    Add event listeners for keypress; begin logic for moving

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const MAP_LEVEL_ONE = [
     [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
     [1, 0, 1, 5, 0, 0, 4, 0, 0, 0, 0, 1, 5, 1, 0, 1],
     [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 0, 3, 1, 0, 0, 0, , 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 const PLAYER_START = {
     lives: 3,
-    creaturesFound: 0
+    creaturesFound: 0,
+    mazePosition: [1, 0]
 }
 
 const gridClasses = ['path', 'wall', 'player', 'enemy', 'obstacle', 'creature']
@@ -32,20 +33,56 @@ const player = PLAYER_START // Sets the player object as a copy of the PLAYER_ST
 const mazeEl = document.querySelector('#maze')
 
 /*----- event listeners -----*/
+document.addEventListener("keydown", keyBehavior);
 
+function keyBehavior(e) {
+  if (e.key === "ArrowUp") {
+    movePlayer('up')
+  } else if (e.key === "ArrowDown") {
+    movePlayer('down')
+  } else if (e.key === "ArrowRight") {
+    movePlayer('right')
+  } else if (e.key === "ArrowLeft") {
+    movePlayer('left')
+  }
+}
 
 /*----- functions -----*/
 
-function movePlayer(){
+// This function checks if the user is trying to make a valid move
+function checkMazeMovement(movementDirection){
+    
+    let desiredCell;
+    if(movementDirection === 'up'){
+        desiredCell = maze[player.mazePosition[0] - 1][player.mazePosition[1]];
+        console.log(desiredCell)
+        if (desiredCell === 1){
+            console.log('Wall; no movement')
+            return false
+        } else {
+            return true
+        }
+    } else {
+        return true
+    }
+}
 
+function movePlayer(direction){
+    if(!checkMazeMovement(direction)){ // TODO - tie into keypress event handler
+        return
+    } else {
+        console.log('Valid move')
+
+    }
 }
 
 function makeMazeDiv(classValue){
     const divEl = document.createElement('div')
-    if(classValue === 1){
-        const randomChoice = Math.random() < 0.5 ? 'wall-asteroid' : 'wall-stars';
-        divEl.classList.add(randomChoice)
-    }
+    // ICEBOX - Allow for more wall styling which doesn't update with each render
+    // if(classValue === 1){
+    //     const randomChoice = Math.random() < 0.5 ? 'wall-asteroid' : 'wall-stars';
+    //     divEl.classList.add(randomChoice)
+    // }
     divEl.classList.add(gridClasses[classValue])
     mazeEl.appendChild(divEl)
 }
